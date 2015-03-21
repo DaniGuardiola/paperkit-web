12e236aa601bf04c12b762782b1c59dae1695c0e
DaniGuardiola <contacto@daniguardiola.me>
2015-03-21 19:58:18 +0100

    Initial

---
+++ reference/resources/script.js
@@ -0,0 +1,23 @@
+/* global paperkit */
+/* exported scrollCollapse, openDiscussion */
+var scrollCollapse;
+
+function contentPagerAction(tab, index) {
+  if (index === 4) {
+    paperkit.fab.hide();
+  } else {
+    paperkit.fab.show();
+  }
+}
+
+function contentPage(index) {
+  var pager = document.getElementById("content-pager");
+  var tabbar = document.getElementById("content-tabbar");
+  pager.moveToPage(index);
+  tabbar.moveIndicatorToTab(index);
+  contentPagerAction(false, index);
+}
+
+function openDiscussion() {
+  contentPage(4);
+}
